6cd0d5ef6873fb689f647fec42fd1444824c3717
Junichi Ito <jit@sonicgarden.jp>
2013-07-20 15:04:04 +0900

    Setup copy-tuner

--- Gemfile
+++ Gemfile
@@ -33,3 +33,4 @@ gem 'bootstrap-sass', '~> 2.3.2.1'
 gem "simple_form", "3.0.0.rc"
 gem 'pg'
 gem 'unicorn'
+gem 'copy_tuner_client', '~> 0.0.1'
---
+++ config/initializers/copy_tuner.rb
@@ -0,0 +1,4 @@
+CopyTunerClient.configure do |config|
+  config.api_key = 'c671c61e246d4335884814c05decc26a7ab2728f769571af'
+  config.host = 'copy-tuner.herokuapp.com'
+end
