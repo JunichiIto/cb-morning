b97e0346d678a2be47dae34d3be48adf35e7e2aa
Junichi Ito <jit@sonicgarden.jp>
2013-08-24 11:03:16 +0900

    Enable to manage keywords

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -69,6 +69,6 @@ class MenuItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def menu_item_params
-      params.require(:menu_item).permit(:name, :category_list, :category_id)
+      params.require(:menu_item).permit(:name, :keyword_list, :category_id)
     end
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,4 @@
 class MenuItem < ActiveRecord::Base
-  acts_as_taggable_on :categories
+  acts_as_taggable_on :keywords
   belongs_to :category
 end
