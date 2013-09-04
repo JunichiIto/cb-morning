1a406bf24e5bc931fc55ab50365e443a8dfe717b
Junichi Ito <jit@sonicgarden.jp>
2013-09-04 12:57:25 +0900

    Update logging gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,8 +49,8 @@ CbMorning::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
-  config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
+  # config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+  # config.logger.level = Logger.const_get(ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].upcase : 'INFO')
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
