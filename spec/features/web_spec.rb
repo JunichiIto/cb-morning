5a4e9cf650ef37a21b3040cbb0513e606c72cde3
Junichi Ito <jit@sonicgarden.jp>
2014-05-01 07:33:15 +0900

    Add feature spec.

--- spec/features/web_spec.rb
+++ spec/features/web_spec.rb
@@ -4,5 +4,28 @@ feature "web" do
   scenario "save and view" do
     visit root_path
     expect(page).to have_content "CB Morning"
+
+    click_link "New Category"
+    fill_in "Name", with: "菓子パン"
+    click_button "Create Category"
+    expect(page).to have_content "Category Was Successfully Created"
+
+    click_link "Menu Items"
+    click_link "New Menu item"
+    fill_in "Name", with: "あんぱん"
+    select "菓子パン", from: "Category"
+    fill_in "Keyword list", with: "あんこ,こしあん"
+    click_button "Create Menu item"
+    expect(page).to have_content "Menu Item Was Successfully Created"
+
+    click_link "Back"
+    expect(page).to have_content "菓子パン"
+    expect(page).to have_content "あんぱん"
+    expect(page).to have_content "あんこ"
+    expect(page).to have_content "こしあん"
+
+    click_link "CB Morning"
+    expect(page).to have_content "菓子パン"
+    expect(page).to have_content "あんぱん"
   end
 end
