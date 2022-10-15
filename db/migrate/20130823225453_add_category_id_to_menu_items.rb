3e41f73a352366ba1127131ee3e6d06b1a9b9fb6
Junichi Ito <jit@sonicgarden.jp>
2022-10-15 11:54:42 +0900

    Renew migration files

--- db/migrate/20130823225453_add_category_id_to_menu_items.rb
+++ db/migrate/20130823225453_add_category_id_to_menu_items.rb
@@ -1,4 +1,4 @@
-class AddCategoryIdToMenuItems < ActiveRecord::Migration
+class AddCategoryIdToMenuItems < ActiveRecord::Migration[5.1]
   def change
     add_column :menu_items, :category_id, :integer
     add_index :menu_items, :category_id
--- db/schema.rb
+++ db/schema.rb
@@ -2,37 +2,37 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_08_13_090055) do
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "position"
   end
 
   create_table "menu_items", force: :cascade do |t|
-    t.string "name", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "category_id"
     t.index ["category_id"], name: "index_menu_items_on_category_id"
   end
 
   create_table "taggings", force: :cascade do |t|
     t.integer "tag_id"
+    t.string "taggable_type"
     t.integer "taggable_id"
-    t.string "taggable_type", limit: 255
+    t.string "tagger_type"
     t.integer "tagger_id"
-    t.string "tagger_type", limit: 255
     t.string "context", limit: 128
     t.datetime "created_at"
     t.index ["context"], name: "index_taggings_on_context"
@@ -41,13 +41,15 @@ ActiveRecord::Schema.define(version: 2018_08_13_090055) do
     t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
     t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
     t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+    t.index ["taggable_type", "taggable_id"], name: "index_taggings_on_taggable"
     t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
     t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
     t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+    t.index ["tagger_type", "tagger_id"], name: "index_taggings_on_tagger"
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
     t.integer "taggings_count", default: 0
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
